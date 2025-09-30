6c0e60849ec67e4695a4b0872da37e2391b2f3e6
wolodya <wolodya@GLaDOS.aperture.lab>
2025-09-30 11:22:40 +0500

    Add model Question

---
+++ src/com/utils/models/Question.java
@@ -0,0 +1,21 @@
+package src.com.utils.models;
+
+public class Question {
+    private String question;
+    private String answer;
+    private Integer id;
+
+    //Getter for question
+    public String getQuestion() {
+        return question;
+    }
+
+    //Getter for answer
+    public String getAnswer() {
+        return answer;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
