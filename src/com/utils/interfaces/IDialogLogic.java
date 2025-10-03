2a31290fae25915105f891a05a4c4f9db2966f1e
sudo rm -rf / <maximlogs2@gmail.com>
2025-10-03 10:20:52 +0500

    add interface IDialogLogic

---
+++ src/com/utils/interfaces/IDialogLogic.java
@@ -0,0 +1,7 @@
+package src.com.utils.interfaces;
+
+public interface IDialogLogic {
+    IQuestion getQA();
+    boolean checkAnswer();
+    String getHelp();
+}
--- src/com/utils/interfaces/IQuestionRepository.java
+++ src/com/utils/interfaces/IQuestionRepository.java
@@ -1,7 +1,5 @@
 package src.com.utils.interfaces;
 
-import src.com.utils.models.Question;
-
 public interface IQuestionRepository {
-    Question getRandomQuestion();
+    IQuestion getRandomQuestion();
 }
