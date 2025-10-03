a5dfadcd79c6bb9654ec598d378d5318db423aed
sudo rm -rf / <maximlogs2@gmail.com>
2025-10-03 10:28:00 +0500

    correct function checkAnswer() in IDialogLogic

--- src/com/utils/interfaces/IDialogLogic.java
+++ src/com/utils/interfaces/IDialogLogic.java
@@ -2,6 +2,6 @@ package src.com.utils.interfaces;
 
 public interface IDialogLogic {
     IQuestion getQA();
-    boolean checkAnswer();
+    boolean checkAnswer(int answer);
     String getHelp();
 }
