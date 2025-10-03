ffcefed2fe8dadbebab38423ac39c61a0c434352
sudo rm -rf / <maximlogs2@gmail.com>
2025-10-03 10:39:35 +0500

    add class DialogLogic

---
+++ src/com/utils/services/DialogLogic.java
@@ -0,0 +1,29 @@
+package src.com.utils.services;
+
+import src.com.utils.interfaces.IDialogLogic;
+import src.com.utils.interfaces.IQuestion;
+import src.com.utils.interfaces.IQuestionRepository;
+
+import java.util.Objects;
+
+public class DialogLogic implements IDialogLogic {
+    IQuestionRepository questionRepository;
+    IQuestion currentQA;
+
+    DialogLogic (IQuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public IQuestion getQA() {
+        return (this.currentQA = questionRepository.getRandomQuestion());
+    }
+
+    public boolean checkAnswer(String answer) {
+        return (Objects.equals(currentQA.getAnswer(), answer));
+    }
+
+    public String getHelp() {
+        return "Это бот для курса ООП - его я вам дам\n" +
+                "А это справочка для бота - её я вам не дам...";
+    }
+}
