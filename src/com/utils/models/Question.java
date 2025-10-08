d58e06845f861295720c9d41323672590a7a3eae
wolodya <vovashagapov@gmail.com>
2025-10-08 16:14:04 +0500

    Add correct models Question And add class QuestionRepository

--- src/com/utils/models/Question.java
+++ src/com/utils/models/Question.java
@@ -1,9 +1,14 @@
 package src.com.utils.models;
 
-public class Question {
+import src.com.utils.interfaces.IQuestion;
+
+public class Question implements IQuestion {
     private String question;
     private String answer;
     private Integer id;
+
+    public Question(int id, String question, String answer) {
+    }
 
     //Getter for question
     public String getQuestion() {
