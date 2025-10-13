13c7e242216c59a63ea7672a0000d419101fb98d
wolodya <vovashagapov@gmail.com>
2025-10-13 18:11:41 +0500

    initialized fields in the constructor fo Question

--- src/com/utils/models/Question.java
+++ src/com/utils/models/Question.java
@@ -8,6 +8,9 @@ public class Question implements IQuestion {
     private Integer id;
 
     public Question(int id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
     }
 
     //Getter for question
